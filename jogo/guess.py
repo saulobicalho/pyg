d88168dfe29f9c2da1f1668cffcd76a7d2026407
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-08-31 11:34:23 -0300

    break/continue

--- jogo/guess.py
+++ jogo/guess.py
@@ -9,12 +9,18 @@ for rodada in range(1, total_de_tentativas+1):
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")

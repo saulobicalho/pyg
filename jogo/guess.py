d64d5c6a70f75d79d3112567695191a0351380c3
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-08-31 11:31:08 -0300

    fix counter

--- jogo/guess.py
+++ jogo/guess.py
@@ -3,7 +3,7 @@ numero_secreto = 32
 total_de_tentativas = 3
 rodada = 1
 
-for rodada in range(1, total_de_tentativas):
+for rodada in range(1, total_de_tentativas+1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)

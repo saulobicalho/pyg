61a4c0007e8ba9266c5b85ec605ef5f6b768ab9d
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-08-31 11:43:57 -0300

    levels

--- jogo/guess.py
+++ jogo/guess.py
@@ -2,8 +2,20 @@ import random
 
 print("welcome")
 numero_secreto = random.randrange(1, 100)
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas+1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
